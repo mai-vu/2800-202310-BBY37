4e37977bdc779e160ed67d2d9707bae3edc2764b
Haurence Li <hli223@my.bcit.ca>
2023-05-17 18:10:28 -0700

    super minor refactor

--- index.js
+++ index.js
@@ -75,8 +75,7 @@ app.use(express.static(__dirname + "/public"));
 app.use('/fuzzySearch', fuzzysearchRouter);
 
 app.get("*", (req, res) => {
-    res.status(404);
-    res.render("404");
+  res.status(404).render("404");
 })
 
 app.listen(port, () => {
