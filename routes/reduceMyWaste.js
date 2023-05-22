2f3a54d29d0113b59d68eb86dd4cccaed08578d5
Tomasz Stojek <stojek604@gmail.com>
2023-05-21 20:23:39 -0700

    Made route for chatgpt

--- routes/reduceMyWaste.js
+++ routes/reduceMyWaste.js
@@ -5,51 +5,46 @@ const router = express.Router()
 const fs = require('fs');
 
 
+//test prompt
+let testPrompt = "give me two things, 1 what do do with expires bananas and what do to do with it when its about to go bad";
 
-//database connection
-const mongodb_database = process.env.MONGODB_DATABASE;
-var {
-    database
-} = include('database');
-const userCollection = database.db(mongodb_database).collection('users');
+//async function to get content from OpenAi API
+//function to for openai completion without test page
+async function sendPrompt(testPrompt) {
+    const response = await openai.createCompletion({
+        model : "text-davinci-003",
+        prompt : testPrompt,
+        max_tokens : 2000,
+        temperature : 0.9,
+    });
+    const parsableJSON = response.data.choices[0].text;
+
+    // const parsedJSON = JSON.parse(parsableJSON);
+
+    console.log(parsableJSON);
+    // console.log(parsedJSON);
+    console.log(parsedJSON.recipes.length);
+    return parsedJSON;
+    
+};
+
+
 
 //Generate recipes based on ingredients list
 router.post('/', async (req, res) => {
-    try {
-        if (!req.session.authenticated) {
-            res.redirect('/?error=' + encodeURIComponent('You must be logged in to view this page. Sign up or log in now'));
-            return;
-        }
-
-        const ingredients = JSON.parse(req.body.ingredients);
-        console.log(ingredients);
-
-        // Retrieve the user's email from the session
-        const email = req.session.email;
-        // Query the database for the user
-        const user = await userCollection.findOne({
-            email: email
-        });
-
-        if (user) {
-
-            const ingredientsJSON = JSON.stringify(ingredients);
-
-            // Render the reduceMyWaste page (reduceMyWaste.ejs) with the matching information 
-            res.render("reduceMyWaste", {
-                email: req.session.email,
-                name: req.session.name,
-                ingredients: ingredientsJSON
-            });
-        } else {
-            // Handle case when user is not found
-            res.status(404).send('User not found');
-        }
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Internal Server Error');
-    }
+   let ingredients = req.session.ingredients;
+   console.log(ingredients);
+   for (let i = 0; i < ingredients.length; i++) {
+       console.log(ingredients[i]);
+   }
+   let chatString = await sendPrompt(testPrompt);
+   console.log(chatString);
+   res.send(chatString);
+   
 });
+
+
+
 
 
 
